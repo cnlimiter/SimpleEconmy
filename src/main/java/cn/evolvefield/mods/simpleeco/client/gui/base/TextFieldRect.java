cf3a4164f26828833112f25ca3d4a404dd30cb79
cnlimiter <cnlimiter@163.com>
2021-06-12 20:12:40 +0800

    gui utils

---
+++ src/main/java/cn/evolvefield/mods/simpleeco/client/gui/base/TextFieldRect.java
@@ -0,0 +1,22 @@
+package cn.evolvefield.mods.simpleeco.client.gui.base;
+
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.util.text.StringTextComponent;
+
+public class TextFieldRect extends TextFieldWidget {
+    /**
+     * Used as the basic text field for anything in the mod.
+     * @param defaultString The initial text rendered on the button.
+     */
+    public TextFieldRect(FontRenderer fontRenderer, int x, int y, int width, int stringLimit, String defaultString) {
+
+        super(fontRenderer, x + 1, y + 2, width, 12, new StringTextComponent(defaultString));
+        setTextColor(-1);
+        setTextColorUneditable(-1);
+        setBordered(true);
+        setMaxLength(stringLimit);
+        insertText(defaultString);
+        setCanLoseFocus(true);
+    }
+}
