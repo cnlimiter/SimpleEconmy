f7bfec0be74c0f1c9d2499f17555869f380ad28f
cnlimiter <cnlimiter@163.com>
2021-06-29 17:29:20 +0800

    add utils

--- src/main/java/cn/evolvefield/mods/simpleeco/utils/ItemsUtil.java
+++ src/main/java/cn/evolvefield/mods/simpleeco/utils/ItemsUtil.java
@@ -1,6 +1,7 @@
 package cn.evolvefield.mods.simpleeco.utils;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
@@ -20,6 +21,27 @@ public class ItemsUtil {
         catch (CommandSyntaxException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void spawnOverflowingStackAtEntity(World world, Entity entity, ItemStack stack) {
+        spawnOverflowingStack(world, (float) entity.getPosition().getX() + 0.5F, (float) entity.getPosition().getY() + 0.5F, (float) entity.getPosition().getZ() + 0.5F, stack);
+    }
+
+    private static void spawnOverflowingStack(World world, float x, float y, float z, ItemStack stack) {
+
+        if (stack.getCount() > stack.getMaxStackSize()) {
+
+            int amountLeft = stack.getCount();
+
+            while (amountLeft > 0) {
+                ItemStack spawnStack = stack.copy();
+                spawnStack.setCount(Math.min(amountLeft, stack.getMaxStackSize()));
+                ItemsUtil.spawnStack(world, x, y, z, spawnStack);
+                amountLeft -= spawnStack.getCount();
+            }
+        }
+
+        else ItemsUtil.spawnStack(world, x, y, z, stack);
     }
 
     public static ItemEntity spawnStackAtLocation(World world, Location location, ItemStack stack) {
